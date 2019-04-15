3ec5dad15f221006faceaff83fb236404b934be0
Elliot <elliot@ayup.agency>
2019-04-15 11:03:28 +0100

    fixed mobile nav

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
+/* Mobile menu class toggles */
 const hamburger = document.getElementById("jsHamburger");
 const searchSection = document.getElementById("jsSearch");
+const searchOuterSection = document.getElementById("jsSearchOuter");
 const mobileNav = document.getElementById("jsMobileMenu");
+const body = document.getElementById("jsBody");
 
 hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("nav--hamburger--active");
   searchSection.classList.toggle("search__form--hide");
+  searchOuterSection.classList.toggle("search__outer--menu");
   mobileNav.classList.toggle("nav--mobile--active");
+  body.classList.toggle("noscroll");
 });
